94678ab70cb33704c6071636188d81b3cad1c431
alex-hladun <alexhladun@gmail.com>
2020-06-08 19:47:17 +0000

    Installed EJS and added examples to express_server.js

--- express_server.js
+++ express_server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
