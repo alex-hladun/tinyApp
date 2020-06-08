b5212a444789e870c8e7cefbcaca5db019ef3196
alex-hladun <alexhladun@gmail.com>
2020-06-08 20:35:34 +0000

    Added routing for individual links

--- express_server.js
+++ express_server.js
@@ -4,16 +4,24 @@ const PORT = 8080;
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 // /urls Route Handler.
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.get("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  let templateVars = { shortURL, longURL };
+  res.render("urls_show", templateVars);
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello!');
